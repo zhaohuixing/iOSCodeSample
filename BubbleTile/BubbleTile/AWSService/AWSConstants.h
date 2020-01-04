3b0f84d40104cbd47bf9f43e7f8edd1f882756b8
Zhaohui Xing <xingzhh@gmail.com>
2020-01-04 14:34:43 -0700

    Remove oboselete code

--- BubbleTile/BubbleTile/AWSService/AWSConstants.h
+++ BubbleTile/BubbleTile/AWSService/AWSConstants.h
@@ -9,8 +9,8 @@
 #ifndef XGADGET_AWSCONSTANTS_h
 #define XGADGET_AWSCONSTANTS_h
 
-#define XGADGET_ACCESS_KEY_ID                   @"AKIAIWA36AM3MVUZB3DA"
-#define XGADGET_SECRET_KEY                      @"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"
+#define XGADGET_ACCESS_KEY_ID                   @"xxxxxxxxxxxxxxxxx"
+#define XGADGET_SECRET_KEY                      @"xxxxxxxxxxxxxxxxx"
 
 #define FLYINGCOW_GAMERESULT_TOPIC              @"BubbleTile_GameResult_Sharing"
 #define FLYINGCOW_GAMERESULT_QUEUE              @"BubbleTile_GameResult_Queue"
